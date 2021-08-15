463ddeff9606cf0266fafc7380ac6e494bddb0dd
Justyn Duthler <justynduthler@gmail.com>
2021-08-15 14:02:15 -0700

    implemented first 0x00-0x0F disassembled

--- Game Boy Emulator/Disassembler.cpp
+++ Game Boy Emulator/Disassembler.cpp
@@ -19,15 +19,44 @@ int gbDisassembler(const char* fileName) {
 		std::ofstream logFile("C:\\Users\\Justyn Duthler\\Desktop\\Game-Boy-Emulator\\logs\\disassemblerLog.txt", std::fstream::out | std::fstream::trunc);
 		if (logFile.is_open()) {
 			for (int i = 0; i < fileSize; i+=1) {
-				logFile << std::setfill('0') << std::setw(2) << std::hex << (0xff & (int)fileMem[i]) << ":";
-				if (i == 0) {
-					std::cout << fileMem[i] << "\n";
-				}
+				logFile << std::setfill('0') << std::setw(2) << std::hex << (0xff & fileMem[i]) << ": ";
 				switch (fileMem[i]) {
-					case 0xc3:
-						logFile << "JP a16\n";
+					case 0x00: // NOP
+						logFile << "NOP\n"; break;
+					case 0x01: // LD BC, d16
+						logFile << "LD BC, d16\n"; break;
+					case 0x02: // LD (BC), A
+						logFile << "LD (BC), A\n"; break;
+					case 0x03: // INC BC
+						logFile << "INC BC\n"; break;
+					case 0x04: // INC B
+						logFile << "INC B\n"; break;
+					case 0x05: // DEC B
+						logFile << "DEC B\n"; break;
+					case 0x06: // LD B, d8
+						logFile << "LD B, d8\n"; break;
+					case 0x07: // RLCA 
+						logFile << "RLCA\n"; break;
+					case 0x08: // LD (a16), SP
+						logFile << "LD (a16), SP\n"; break;
+					case 0x09: // ADD HL, BC
+						logFile << "ADD HL, BC\n"; break;
+					case 0x0A: // LD A, (BC)
+						logFile << "LD A, (BC)\n"; break;
+					case 0x0B: // DEC BC
+						logFile << "DEC BC\n"; break;
+					case 0x0C: // INC C
+						logFile << "INC C\n"; break;
+					case 0x0D: // DEC C
+						logFile << "DEC C\n"; break;
+					case 0x0E: // LD C, d8
+						logFile << "LD C, d8\n"; break;
+					case 0x0F: // RRCA
+						logFile << "RRCA\n"; break;
+					case 0xc3: // JP NZ, a16
+						logFile << "JP NZ, a16\n"; break;
 					default:
-						logFile << "";
+						logFile << "\n"; break;
 				}
 			}
 		} else {
