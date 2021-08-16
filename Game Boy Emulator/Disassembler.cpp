7002f4a558af3356563314e2cba847f2095e8b55
Justyn Duthler <justynduthler@gmail.com>
2021-08-16 13:49:35 -0700

    More opcodes

--- Game Boy Emulator/Disassembler.cpp
+++ Game Boy Emulator/Disassembler.cpp
@@ -53,6 +53,106 @@ int gbDisassembler(const char* fileName) {
 						logFile << "LD C, d8\n"; break;
 					case 0x0F: // RRCA
 						logFile << "RRCA\n"; break;
+
+					case 0x10: // STOP
+						logFile << "STOP\n"; break;
+					case 0x11: // LD DE, d16
+						logFile << "LD DE, d16\n"; break;
+					case 0x12: // LD (DE), A
+						logFile << "LD (DE), A\n"; break;
+					case 0x13: // INC DE
+						logFile << "INC DE\n"; break;
+					case 0x14: // INC D
+						logFile << "INC D\n"; break;
+					case 0x15: // DEC D
+						logFile << "DEC D\n"; break;
+					case 0x16: // LD D, d8
+						logFile << "LD D, d8\n"; break;
+					case 0x17: // RLA 
+						logFile << "RLA\n"; break;
+					case 0x18: // JR s8
+						logFile << "JR s8\n"; break;
+					case 0x19: // ADD HL, DE
+						logFile << "ADD HL, DE\n"; break;
+					case 0x1A: // LD A, (DE)
+						logFile << "LD A, (DE)\n"; break;
+					case 0x1B: // DEC DE
+						logFile << "DEC DE\n"; break;
+					case 0x1C: // INC E
+						logFile << "INC E\n"; break;
+					case 0x1D: // DEC E
+						logFile << "DEC E\n"; break;
+					case 0x1E: // LD E, d8
+						logFile << "LD E, d8\n"; break;
+					case 0x1F: // RRA
+						logFile << "RRA\n"; break;
+
+					case 0x20: // JR NZ, s8
+						logFile << "JR NZ, s8\n"; break;
+					case 0x21: // LD HL, d16
+						logFile << "LD HL, d16\n"; break;
+					case 0x22: // LD (HL+), A
+						logFile << "LD (HL+), A\n"; break;
+					case 0x23: // INC HL
+						logFile << "INC HL\n"; break;
+					case 0x24: // INC H
+						logFile << "INC H\n"; break;
+					case 0x25: // DEC H
+						logFile << "DEC H\n"; break;
+					case 0x26: // LD H, d8
+						logFile << "LD H, d8\n"; break;
+					case 0x27: // DAA 
+						logFile << "DAA\n"; break;
+					case 0x28: // JR Z, s8
+						logFile << "JR Z, s8\n"; break;
+					case 0x29: // ADD HL, HL
+						logFile << "ADD HL, HL\n"; break;
+					case 0x2A: // LD A, (HL+)
+						logFile << "LD A, (HL+)\n"; break;
+					case 0x2B: // DEC HL
+						logFile << "DEC HL\n"; break;
+					case 0x2C: // INC L
+						logFile << "INC L\n"; break;
+					case 0x2D: // DEC L
+						logFile << "DEC L\n"; break;
+					case 0x2E: // LD L, d8
+						logFile << "LD L, d8\n"; break;
+					case 0x2F: // CPL
+						logFile << "CPL\n"; break;
+					
+					case 0x30: // JR NC, s8
+						logFile << "JR NC, s8\n"; break;
+					case 0x31: // LD SP, d16
+						logFile << "LD SP, d16\n"; break;
+					case 0x32: // LD (HL-), A
+						logFile << "LD (HL-), A\n"; break;
+					case 0x33: // INC SP
+						logFile << "INC HL\n"; break;
+					case 0x34: // INC (HL)
+						logFile << "INC (HL)\n"; break;
+					case 0x35: // DEC (HL)
+						logFile << "DEC (HL)\n"; break;
+					case 0x36: // LD (HL), d8
+						logFile << "LD (HL), d8\n"; break;
+					case 0x37: // SCF 
+						logFile << "SCF\n"; break;
+					case 0x38: // JR C, s8
+						logFile << "JR C, s8\n"; break;
+					case 0x39: // ADD HL, SP
+						logFile << "ADD HL, SP\n"; break;
+					case 0x3A: // LD A, (HL-)
+						logFile << "LD A, (HL-)\n"; break;
+					case 0x3B: // DEC SP
+						logFile << "DEC SP\n"; break;
+					case 0x3C: // INC A
+						logFile << "INC A\n"; break;
+					case 0x3D: // DEC A
+						logFile << "DEC A\n"; break;
+					case 0x3E: // LD A, d8
+						logFile << "LD A, d8\n"; break;
+					case 0x3F: // CCF
+						logFile << "CCF\n"; break;
+					
 					case 0xc3: // JP NZ, a16
 						logFile << "JP NZ, a16\n"; break;
 					default:
